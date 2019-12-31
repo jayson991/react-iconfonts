abdc51283b9451c57192ee9ac0204a88a6bbad93
Jayson Wu <wulingjie991@outlook.com>
2019-12-31 22:53:23 +0800

    update packages to remove vulnerabilities warnings

--- example/react-demo/src/App.js
+++ example/react-demo/src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import './App.css';
-import './assets/icons/iconfont.css';
-import './assets/icons/iconfont.js';
-import IconFont from 'react-iconfonts';
+import React from "react";
+import "./App.css";
+import "./assets/icons/iconfont.css";
+import "./assets/icons/iconfont.js";
+import IconFont from "react-iconfonts";
 
 function App() {
   return (
     <div className="App">
       <div>
-        Hello <IconFont type="icon" name="hasSelected" color="f00" size="30" /> FontIcon
+        Hello
+        <IconFont type="icon" name="circle-unchecked" color="f00" size="30" />
+        FontIcon
       </div>
       <div>
-        Hello <IconFont type="icon" name="notSelected" color="f00" size="30" /> FontIcon
+        Hello
+        <IconFont type="svg" name="circle-unchecked" color="f00" size="30" />
+        SvgIcon
       </div>
 
       <div>
-        Hello <IconFont type="svg" name="hasSelected" color="f00" size="30" /> SvgIcon
+        Hello
+        <IconFont type="icon" name="circle-checked" color="f00" size="30" />
+        FontIcon
       </div>
       <div>
-        Hello <IconFont type="svg" name="notSelected" color="f00" size="30" /> SvgIcon
+        Hello
+        <IconFont type="svg" name="circle-checked" color="f00" size="30" />
+        SvgIcon
+      </div>
+      <div>
+        Hello
+        <IconFont type="icon" name="tick-checked" color="f00" size="30" />
+        FontIcon
+      </div>
+      <div>
+        Hello
+        <IconFont type="svg" name="tick-checked" color="f00" size="30" />
+        SvgIcon
       </div>
     </div>
   );
